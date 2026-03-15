b07b6d982214ad8d156a883c6098b46e417500fe
Hafoozi <toadgranola+github@gmail.com>
2026-03-15 16:34:35 -0400

    Fix Database type and jobs source across codebase

--- types/database.ts
+++ types/database.ts
@@ -18,6 +18,10 @@ export type AllowanceFrequency = 'weekly'
 
 export interface Database {
   public: {
+    Views: { [_ in never]: never }
+    Functions: { [_ in never]: never }
+    Enums: { [_ in never]: never }
+    CompositeTypes: { [_ in never]: never }
     Tables: {
       families: {
         Row: {
